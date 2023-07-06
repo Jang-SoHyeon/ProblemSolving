2ce3f3379adfd31cd4884902e56b66528c982769
Jang-SoHyeon <jangso6593@gmail.com>
2023-07-06 15:29:30 +0900

    baekjoon

--- src/Java_S_4949.java
+++ src/Java_S_4949.java
@@ -12,11 +12,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Java_S_60901632{
+public class Java_S_4949 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
