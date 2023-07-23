731246c9fb351fa6301f102560d6c1ad75c45351
Jang-SoHyeon <jangso6593@gmail.com>
2023-07-23 23:18:10 +0900

    baekjoon

--- src/Silver/Java_S_1021.java
+++ src/Silver/Java_S_1021.java
@@ -1,2 +1,60 @@
-package Silver;public class Java_S_10211 {
+package Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Java_S_1021{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        Deque<Integer> que = new ArrayDeque<>();
+        Deque<Integer> cpyQue = new ArrayDeque<>();
+        int count = 0; //출력할 값
+        int countLeft  ; // 왼쪽부터 해당 값까지 몇 개인지
+        int countRight  ; // 오른쪽부터 해당 값까지 몇 개인지
+
+        for(int i=0; i<N; i++){
+            que.add(i+1);
+        }
+        cpyQue.addAll(que);
+//        System.out.println(cpyQue);
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<M; i++){
+            int find = Integer.parseInt(st.nextToken());
+            countLeft = countRight =  0;
+
+            while(que.getFirst() != find){
+                que.addLast(que.removeFirst());
+                countLeft++;
+            }
+            while(cpyQue.getLast() != find){
+                cpyQue.addFirst(cpyQue.removeLast());
+                countRight++;
+            }
+            if(countLeft <= countRight){
+                count += countLeft;
+                que.removeFirst();
+                cpyQue.clear();
+                cpyQue.addAll(que) ;
+            }
+            else{
+                count += countRight+1;
+                cpyQue.removeLast();
+                que.clear();
+                que.addAll(cpyQue) ;
+            }
+
+
+        }
+        System.out.println(count);
+
+
+    }
 }
--- src/Silver/Java_S_2164.java
+++ src/Silver/Java_S_2164.java
@@ -1,5 +1,32 @@
+package Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Java_S_2164 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> queue = new ArrayDeque<>();
+
+        for(int i=0; i<N;i++){
+            queue.add(i+1);
+        }
+        boolean first = false; //false이면 삭제할 값 true이면 뒤로 갈 값
+        while(queue.size() != 1){
+            if(!first) {
+                queue.remove();
+                first = true;
+            }
+            else{
+                queue.addLast(queue.poll());
+                first = false;
+            }
+        }
+        System.out.println(queue.peek());
+
     }
 }
